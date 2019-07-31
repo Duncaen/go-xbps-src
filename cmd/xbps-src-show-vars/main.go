695617b95671b364892f3993bbfbe7b57f79f6ac
Duncan Overbruck <mail@duncano.de>
2019-07-31 14:44:22 +0200

    cmd/xbps-src-show-vars: update

--- cmd/xbps-src-show-vars/main.go
+++ cmd/xbps-src-show-vars/main.go
@@ -4,18 +4,35 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/Duncaen/go-xbps-src/runtime"
 	"github.com/Duncaen/go-xbps-src/template"
 )
 
 var (
-	machine = flag.String("machine", "x86_64", "architecture")
-	targetMachine = flag.String("target-machine", "", "target architecture")
+	distdir = flag.String("distdir", "/home/duncan/void-packages", "distdir")
+	arch = flag.String("arch", "x86_64", "architecture")
+	cross = flag.String("cross", "", "cross architecture")
+	variables = []string{
+		"version",
+		"revision",
+		"archs",
+		"arch",
+		"broken",
+		"nocross",
+		"hostmakedepends",
+		"makedepends",
+		"depends",
+	}
 )
 
 func main() {
 	flag.Parse()
-	env := template.Environment(*machine, *targetMachine)
+	run, err := runtime.New(*distdir)
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, path := range flag.Args() {
 		t, err := template.ParseFile(path)
 		if err != nil {
@@ -23,9 +40,22 @@ func main() {
 			continue
 		}
 		log.Printf("Evaluating: %s\n", path)
-		vars, err := t.Eval(env)
-		for k, v := range vars {
-			fmt.Printf("%s=%q\n", k, v)
+		vs, err := t.Eval(run, *arch, *cross)
+		if err != nil {
+			log.Fatal(err)
 		}
+		for _, kv := range vs {
+			pkgname := kv["pkgname"]
+			for _, s := range variables {
+				val := strings.Join(strings.Fields(kv[s]), " ")
+				if val == "" {
+					continue
+				}
+				fmt.Printf("%s: %s=%q\n", pkgname, s, val)
+			}
+		}
+		// for _, sv := range vars.SubPackages {
+		// 	fmt.Printf("%s: depends=%v\n", sv.PkgName, sv.Depends)
+		// }
 	}
 }
